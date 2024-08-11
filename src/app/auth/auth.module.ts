85b375d1ee4cf4c8ffd08aed4075b7c6830c1739
Luis Pablo Nieto Gil <56002574+lpng-plenge@users.noreply.github.com>
2024-08-11 14:47:42 -0600

    HTML Navegacion Heroes y HTML de AUTH

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -14,8 +16,9 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
     RegisterPageComponent
   ],
   imports: [
-    CommonModule,
     AuthRoutingModule,
+    CommonModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LayoutPageComponent {
+  public sideBarItems = [
+    {label: 'List', icon: 'label', url: './list'},
+    {label: 'Add', icon: 'add', url: './new-hero'},
+    {label: 'Search', icon: 'search', url: './search'}
+  ]
 
 }
