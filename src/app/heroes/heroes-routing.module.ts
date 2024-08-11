9018202635df007f0765750fffc4672bc4469dbf
Luis Pablo Nieto Gil <56002574+lpng-plenge@users.noreply.github.com>
2024-08-11 12:41:46 -0600

    Layout and LazyLoad Children Pages

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+
+const routes: Routes = [
+  {
+    //localhost:4200/heroes
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewPageComponent
+      },
+      {
+        path: 'search-page',
+        component: SearchPageComponent
+      },
+      {
+        path: 'edit/:id',
+        component: NewPageComponent
+      },
+      {
+        path: 'list',
+        component: ListPageComponent
+      },
+      {
+        /*Esta ruta se le conoce como comodin
+        Este pad se colocal al final porque luiego no entra ninguna otra ruta*/
+        path: ':id',
+        component: HeroPageComponent
+      },
+
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HeroesRoutingModule { }
