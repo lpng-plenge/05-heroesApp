a0f5163815804784240463b75f94d3ec103f79b3
Luis Pablo Nieto Gil <56002574+lpng-plenge@users.noreply.github.com>
2025-01-19 13:55:50 -0600

    Nuew Page Form G & C

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
-    selector: 'heroes-new-page',
-    templateUrl: './new-page.component.html',
-    styles: ``,
-    standalone: false
+  selector: 'heroes-new-page',
+  templateUrl: './new-page.component.html',
+  styles: ``,
+  standalone: false,
 })
 export class NewPageComponent {
+  //Formulario reactivo
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', {nonNullable:true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
+  ];
 }
